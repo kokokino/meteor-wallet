2c331b16899e122d5b8c29ef6b3b8a3d4800515d
Aaron Rosenzweig <aaron@chatnbike.com>
2022-03-26 22:13:59 -0400

    Removed autopublish - doing manual publish - for security reasons
    We should not autopublish the entire DB to Minimongo, only what we need. In this case, we are still publishing the entire DB but we are specifically doing it (conscious of it).
    
    Signed-off-by: Aaron Rosenzweig <aaron@chatnbike.com>

--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {memo} from "react";
 import {ContactsCollection} from "../api/ContactsCollection";
-import {useTracker} from 'meteor/react-meteor-data';
+import {useSubscribe, useFind} from 'meteor/react-meteor-data';
 import { ErrorAlert } from "./components/ErrorAlert";
 import { SuccessAlert } from "./components/SuccessAlert";
 
 export const ContactList = () => {
   const [error, setError] = React.useState("");
   const [success, setSuccess] = React.useState("");
+
+  const isLoading = useSubscribe('allContacts');
+
+  const contacts = useFind(() => ContactsCollection.find({}, { sort: { createdAt: -1 }}));
 
   const showError = (message) => {
     setError(message);
@@ -22,10 +26,6 @@ export const ContactList = () => {
     }, 5000);
   }
 
-  const contacts = useTracker(() => {
-    return ContactsCollection.find({}, { sort: { createdAt: -1 }}).fetch();
-  });
-
   const removeContact = (evt, _id) => {
     evt.preventDefault();
     Meteor.call('contacts.remove', {contactId: _id}, (errorResponse) => {
@@ -39,6 +39,41 @@ export const ContactList = () => {
     });
   };
 
+  if (isLoading()) {
+    return (
+      <div className="mt-10">
+        <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">
+          Loading...
+        </h3>
+      </div>
+    )
+  }
+
+  const ContactItem = memo(({contact}) => {
+    return (
+      <li className="py-4 flex items-center justify-between space-x-3">
+        <div className="min-w-0 flex-1 flex items-center space-x-3">
+          <div className="flex-shrink-0">
+            <img className="h-10 w-10 rounded-full" src={contact.imageUrl} alt="" />
+          </div>
+          <div className="min-w-0 flex-1">
+            <p className="text-sm font-medium text-gray-900 truncate">{contact.name}</p>
+            <p className="text-sm font-medium text-gray-500 truncate">{contact.email}</p>
+          </div>
+          <div>
+            <a
+              href=""
+              onClick={(evt) => removeContact(evt, contact._id)}
+              className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
+            >
+              Remove
+            </a>
+          </div>
+        </div>
+      </li>
+    )
+  });
+
   return (
     <div>
       <div className="mt-10">
@@ -48,27 +83,8 @@ export const ContactList = () => {
         {error && <ErrorAlert message={error} />}      
         {success && <SuccessAlert message={success} />}
         <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
-          {contacts.map((person, personIdx) => (
-            <li key={personIdx} className="py-4 flex items-center justify-between space-x-3">
-              <div className="min-w-0 flex-1 flex items-center space-x-3">
-                <div className="flex-shrink-0">
-                  <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
-                </div>
-                <div className="min-w-0 flex-1">
-                  <p className="text-sm font-medium text-gray-900 truncate">{person.name}</p>
-                  <p className="text-sm font-medium text-gray-500 truncate">{person.email}</p>
-                </div>
-                <div>
-                  <a
-                    href=""
-                    onClick={(evt) => removeContact(evt, person._id)}
-                    className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
-                  >
-                    Remove
-                  </a>
-                </div>
-              </div>
-            </li>
+          {contacts.map((contact) => (
+            <ContactItem key ={contact._id} contact = {contact} />
           ))}
         </ul>
       </div>
